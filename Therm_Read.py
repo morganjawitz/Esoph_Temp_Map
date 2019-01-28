b619bacdf90cb19b2b18a115dd428a9550a354af
morganjawitz <46722584+morganjawitz@users.noreply.github.com>
2019-01-27 18:19:28 -0800

    added \r to print command

--- Therm_Read.py
+++ Therm_Read.py
@@ -41,5 +41,5 @@ R7_Temp = np.interp(R7_ref,RtR,T_C)
 #print('R_ref =' + str(R7_ref))
 
 while True:
- print('R7 Temperature =' + str(R7_Temp) + 'C')
+ print('R7 Temperature =' + str(R7_Temp) + 'C\r')
  time.sleep(0.5)
