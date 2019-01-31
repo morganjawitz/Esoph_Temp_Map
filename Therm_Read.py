73a2b876fe09d6f3b2f6f658de921e13afaf2bf8
morganjawitz <morgan.jawitz@gmail.com>
2019-01-30 18:20:04 -0800

    Update Therm_Read.py

--- Therm_Read.py
+++ Therm_Read.py
@@ -40,7 +40,7 @@ while True:
  	#print(chan_call)
  	#creating pin object from analog input
  	pin_num = str(mcp) + str(MCP) + "." + str(i)
- 	print(pin_num)
+ 	print(str(pin_num) + "\r", end="")
  	chan = AnalogIn(mcp, MCP.P7) #need to find way to loop through pin number
  	volt = chan.voltage
  	#print(chan.voltage)
