84761b61413cf8f78ef31b46d14f605583739d30
morganjawitz <morgan.jawitz@gmail.com>
2019-02-08 19:30:13 -0800

    Update Graph_Gen.py
    plotting 10s of therm data

--- Graph_Gen.py
+++ Graph_Gen.py
@@ -1,6 +1,12 @@
 import numpy as np
 from matplotlib import pyplot as pyplot
 from matplotlib import animation
-from Therm_Read_Test import Temp_Read
+from Therm_Read_Test import Temp_Read #calling thermistor read function
 
-print(Temp_Read(1))
+for i in range(10)
+	y = Temp_read(1)
+	pyplot.scatter(i,y)
+	pyplot.pause(0.05)
+pyplot.show()
+
+#print(Temp_Read(1))
