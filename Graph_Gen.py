e1961ebf5210f4f7b05f371be61257ffb7bc336b
morganjawitz <morgan.jawitz@gmail.com>
2019-02-08 20:03:01 -0800

    Update Graph_Gen.py
    added 3D matplot lib

--- Graph_Gen.py
+++ Graph_Gen.py
@@ -1,6 +1,5 @@
 import numpy as np
-import datetime as dt
-import time
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import animation
 from Therm_Read_Test import Temp_Read #calling thermistor read function
