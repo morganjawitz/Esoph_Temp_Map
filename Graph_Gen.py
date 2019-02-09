76d92eac24485ad535927c9c4aecc123e6b9734d
morganjawitz <morgan.jawitz@gmail.com>
2019-02-08 19:25:13 -0800

    Update Graph_Gen.py

--- Graph_Gen.py
+++ Graph_Gen.py
@@ -1,6 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as pyplot
 from matplotlib import animation
-from Therm_Read_Test.py import Temp_Read
+from Therm_Read_Test import Temp_Read
 
 print(Temp_Read(1))
