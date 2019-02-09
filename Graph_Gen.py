b973ce6dba0b134bfc6cc137c97bf30cbead9946
morganjawitz <morgan.jawitz@gmail.com>
2019-02-08 20:00:33 -0800

    Update Graph_Gen.py
    attempting plot cylinder

--- Graph_Gen.py
+++ Graph_Gen.py
@@ -11,28 +11,22 @@ ax = fig.add_subplot(1,1,1)
 xs = []
 ys = []
 
+#need to initiate a 3D cylinder graph
+#Next pick 8 3D points on graph to represent thermistors
+#then label each point with the therm reading
+
 def animate(i,xs,ys):
 	#reading temperature
 	Temp = Temp_Read(1)
 	
-	#adding x and y to lists
-	t = str(time.strftime('%l:%M:%S'))
-	xs.append(t)
-	ys.append(Temp)
+	#plotting cylinder
+	x = np.linspace(-1,1,100)
+	z = np.linespace(0,3,100)
+	Xc, Zc = np.meshgrid(x,z)
+	Yc = np.sqrt(1-Xc**2)
 
-	#limt x and y list to 20 items
-	xs = xs[-20:]
-	ys = ys[-20:]
 
-	#Draw x and y list
-	ax.clear()
-	ax.plot(xs, ys)
-
-	#Format plot
-	plt.xticks(rotation=45, ha='right')
-	plt.subplots_adjust(bottom=0.30)
-	plt.title('Therm_1 Reading vs Time')
-	plt.ylabel('Temperature (deg C)')
+	
 
 #set up plot to call animate() function periodically
 ani = animation.FuncAnimation(fig, animate, fargs=(xs,ys), interval=1000)
