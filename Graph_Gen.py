219fc6913480800c85a07d3614a76d98c0682870
morganjawitz <morgan.jawitz@gmail.com>
2019-02-08 20:37:51 -0800

    Update Graph_Gen.py

--- Graph_Gen.py
+++ Graph_Gen.py
@@ -51,7 +51,7 @@ def animate(i,xs,ys,zs):
 	
 
 #set up plot to call animate() function periodically
-ani = animation.FuncAnimation(fig, animate, fargs = (xs, ys, zs) interval=1000)
+ani = animation.FuncAnimation(fig, animate, fargs = (xs, ys, zs), interval=1000)
 plt.show()
 
 
