e70f3e14cc8c19fc7b0a3c94f9de225623faee5a
morganjawitz <morgan.jawitz@gmail.com>
2019-02-08 19:41:02 -0800

    Update Graph_Gen.py

--- Graph_Gen.py
+++ Graph_Gen.py
@@ -1,12 +1,43 @@
 import numpy as np
-from matplotlib import pyplot as pyplot
+import datetime as dt
+import matplotlib.pyplot as plt
 from matplotlib import animation
 from Therm_Read_Test import Temp_Read #calling thermistor read function
 
-for i in range(10)
-	y = Temp_read(1)
-	pyplot.scatter(i,y)
-	pyplot.pause(0.05)
-pyplot.show()
+#initalizing graph
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+xs = []
+ys = []
+
+def animate(i,xs,ys):
+	#reading temperature
+	Temp = Temp_Read(1)
+	
+	#adding x and y to lists
+	xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+	ys.append(Temp)
+
+	#limt x and y list to 20 items
+	xs = xs[-20:]
+	ys = ys[-20:]
+
+	#Draw x and y list
+	ax.clear()
+	ax.plot(xs, ys)
+
+	#Format plot
+	plt.xticks(rotation=45, ha='right')
+	plt.subplots_adjust(bottom=0.30)
+	plt.title('Therm_1 Reading vs Time')
+	plt.ylabel('Temperature (deg C)')
+
+#set up plot to call animate() function periodically
+ani = animation.FuncAnimation(fig, animate, fargs=(xs,ys), interval=1000)
+plt.show()
+
+
+
+
 
 #print(Temp_Read(1))
