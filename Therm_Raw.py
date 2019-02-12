66e933a4eb6609171b821b355047621694a381a8
morganjawitz <morgan.jawitz@gmail.com>
2019-02-12 11:41:57 -0800

    Update Therm_Raw.py

--- Therm_Raw.py
+++ Therm_Raw.py
@@ -2,10 +2,11 @@ from Therm_Read_Test import Temp_Read
 import numpy as np
 import time
 
-Temps = np.zeros(4, dtype=int)
+Temps = [0,0,0,0]
 
 while True:
 	for i in range(0,3):
 		Temps[i] = Temp_Read(i)
+	
 	print(str(Temps) + "\r", end="" )
 	time.sleep(0.5)
