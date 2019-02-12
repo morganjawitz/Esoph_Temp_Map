f8e7a3dc760313fdd81b5a49566d3198e5dea5ab
morganjawitz <morgan.jawitz@gmail.com>
2019-02-12 11:17:11 -0800

    Update Therm_Raw.py

--- Therm_Raw.py
+++ Therm_Raw.py
@@ -7,5 +7,5 @@ Temps = np.zeros(4, dtype=int)
 while True:
 	for i in range(0,3):
 		Temps[i] = Temp_Read(i)
-	print(str(Temps) + "/r", end="" )
+	print(str(Temps) + "\r", end="" )
 	time.sleep(0.5)
