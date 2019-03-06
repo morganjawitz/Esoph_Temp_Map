c4982e67f5f1b137a52e26fadc6bea20f1439de2
morganjawitz <morgan.jawitz@gmail.com>
2019-03-05 21:46:49 -0800

    Update Graph_Gen.py

--- Graph_Gen.py
+++ Graph_Gen.py
@@ -79,8 +79,8 @@ def animate(i,xs,ys,zs,Xc,Zc,Yc,surf1,surf2):
 	#surf2.remove()
 
 	#plot the surface with new colors
-	surf1 = ax.plot_surface(Xc, Yc, Zc, alpha=0.2, rstride=rstride, cstride=cstride, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-	surf2 = ax.plot_surface(Xc, -Yc, Zc, alpha=0.2, rstride=rstride, cstride=cstride, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+	#surf1 = ax.plot_surface(Xc, Yc, Zc, alpha=0.2, rstride=rstride, cstride=cstride, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+	#surf2 = ax.plot_surface(Xc, -Yc, Zc, alpha=0.2, rstride=rstride, cstride=cstride, cmap=cm.coolwarm, linewidth=0, antialiased=False)
 
 
 	#ax.plot_wireframe(Xc,Yc, Zc, rstride = rstride, cstride = cstride)
