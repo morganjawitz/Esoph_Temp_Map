f710454285de3fbe16cf2a200b63edfe80b27f49
morganjawitz <morgan.jawitz@gmail.com>
2019-03-10 12:31:10 -0700

    Update Graph_Gen.py

--- Graph_Gen.py
+++ Graph_Gen.py
@@ -83,8 +83,8 @@ def animate(i,xs,ys,zs,Xc,Zc,Yc):
 
 
 	# Building temperature location matrix
-	x_dim = np.linspace(0,100,100)
-	y_dim = np.linspace(0,100,100)
+	x_dim = np.linspace(0,100,101)
+	y_dim = np.linspace(0,100,101)
 	Tx,Ty = np.meshgrid(x_dim,y_dim)
 	#Tx,Ty = np.mgrid[0:100:100j, 0:100:100j]\
 	print(Tx)
