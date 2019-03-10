6f52215fa55a71e5a3e65b82f511666799d28b0e
morganjawitz <morgan.jawitz@gmail.com>
2019-03-10 12:40:12 -0700

    Update Graph_Gen.py

--- Graph_Gen.py
+++ Graph_Gen.py
@@ -87,8 +87,8 @@ def animate(i,xs,ys,zs,Xc,Zc,Yc):
 	y_dim = np.linspace(0,100,101)
 	Tx,Ty = np.meshgrid(x_dim,y_dim)
 	#Tx,Ty = np.mgrid[0:100:100j, 0:100:100j]\
-	print(Tx)
-	print(Tx.shape)
+	print(Ty)
+	print(Ty.shape)
 	#print(Ty)
 
 	#listing the cordinates with the T matrix where known temps are for positive Yc plot
